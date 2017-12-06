2cb17d1faabd90001fd2a8b03d667bc309ab43f4
Fedde Murua <fedde.murua@gmail.com>
2017-12-06 19:17:51 -0300

    avances usuario + detalles establecimiento

--- modulo_I/models.py
+++ modulo_I/models.py
@@ -57,7 +57,7 @@ class Domicilio(models.Model):
     comarca = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return "%s - %s - %s" % (self.barrio, self.calle, self.nro)
+        return "B° %s, %s  N°%s" % (self.barrio, self.calle, self.nro)
 
 
 '''
@@ -70,6 +70,17 @@ class AmbitoDependencia(models.Model):
     municipalidad = models.CharField(max_length=50, blank=True, null=True)
     centro_que_depende = models.CharField(max_length=50, blank=True, null=True)
 
+    def __str__(self):
+        if self.municipalidad and self.centro_que_depende:
+            return "Categoria:%s - Municipalidad:%s - Centro del que Depende:%s" % \
+            (self.categoria, self.municipalidad, self.centro_que_depende)
+        elif self.municipalidad:
+            return "Categoría:%s - Municipalidad:%s" % (self.categoria, self.municipalidad)
+        elif self.centro_que_depende:
+            return "Centro del que Depende:%s - Municipalidad:%s" % (self.centro_que_depende, self.municipalidad)
+        else:
+            return "Categoría:%s" % self.categoria
+
 
 class CaracteristicasGenerales(models.Model):
     cant_quirofanos = models.IntegerField(default=0,validators=[MinValueValidator(0)])
@@ -78,12 +89,20 @@ class CaracteristicasGenerales(models.Model):
     poblacion_promedio_atendida_365 = models.FloatField(default=0) # (al año)
     via_acceso = MultiSelectField(choices=Accesos)
 
+    def __str__(self):
+        return "Cant. Quirófanos:%s || N° camas:%s || Via Acceso:%s" % \
+        (self.cant_quirofanos, self.nro_camas, self.via_acceso)
+
 
 class ResiduoGenerador(models.Model):
     tipo = models.CharField(max_length=80, choices=TiposResiduos)
     volumen_mensual_estimado = models.CharField(max_length=50)
     kgs_mensual_estimado = models.CharField(max_length=50)
     establecimiento_generador = models.ForeignKey('EstablecimientoGenerador', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Tipo:%s - Volumen Mensual Estimado:%s - Kgs Mensual Estimado:%s" % \
+        (self.tipo, self.volumen_mensual_estimado, self.kgs_mensual_estimado)
 
 
 class AcopioTransitorio(models.Model):
@@ -92,10 +111,25 @@ class AcopioTransitorio(models.Model):
     camara_frio = models.BooleanField()
     dimension_camara_frio = models.CharField(max_length=50, blank=True, null=True)
 
+    def __str__(self):
+        if self.sector_interno_p_estacionar and self.camara_frio:
+            return "Dimensión Sector interno para estacionar:%s - Dmensión Cámara Frío:%s" % \
+            (self.dimension_sector, self.dimension_camara_frio)
+        elif self.sector_interno_p_estacionar:
+            return "Dimensión Sector interno para estacionar:%s" % (self.dimension_sector)
+        elif self.camara_frio:
+            return "Dimensión Cámara Frío:%s" % (self.dimension_camara_frio)
+        else:
+            return ""
+
+
 
 class ViaAccesoSector(models.Model):
-    acopio_transitorio = models.ForeignKey('AcopioTransitorio')
+    acopio_transitorio = models.ForeignKey('AcopioTransitorio', on_delete=models.CASCADE)
     tipo =MultiSelectField(choices=Acceso_sector_acopio)
+
+    def __str__(self):
+        return "%s - %s" % (self.acopio_transitorio, self.tipo)
 
 
 class EstablecimientoGenerador(models.Model):
@@ -103,17 +137,17 @@ class EstablecimientoGenerador(models.Model):
     razon_social = models.CharField(max_length=50)
     telefono = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, blank=True, null=True)
-    director_responsable = models.ForeignKey('Cliente', related_name='director_responsable')
-    responsable_residuos = models.ForeignKey('Persona', related_name='responsable_residuos')
-    responsable_suplente = models.ForeignKey('Persona', related_name='responsable_suplente')
-    responsable_tecnico = models.ForeignKey('Persona', related_name='responsable_tecnico')
+    director_responsable = models.ForeignKey('Cliente', related_name='director_responsable', on_delete=models.CASCADE)
+    responsable_residuos = models.ForeignKey('Persona', related_name='responsable_residuos', on_delete=models.CASCADE)
+    responsable_suplente = models.ForeignKey('Persona', related_name='responsable_suplente', on_delete=models.CASCADE)
+    responsable_tecnico = models.ForeignKey('Persona', related_name='responsable_tecnico', on_delete=models.CASCADE)
     fecha_vinculo = models.DateField() #fecha deL Vinculo que se confeccionó el formulario
     fecha = models.DateField(default=now)
     observaciones_comentarios = models.TextField(max_length=200, default='', blank=True, null=True)
-    caract_generales = models.ForeignKey('CaracteristicasGenerales')
+    caract_generales = models.ForeignKey('CaracteristicasGenerales', on_delete=models.CASCADE)
     domicilio = models.ForeignKey('Domicilio', on_delete=models.CASCADE)
-    ambito_dependencia = models.ForeignKey('AmbitoDependencia')
-    via_acceso = models.ForeignKey('ViaAccesoSector')
+    ambito_dependencia = models.ForeignKey('AmbitoDependencia', on_delete=models.CASCADE)
+    via_acceso = models.ForeignKey('ViaAccesoSector', on_delete=models.CASCADE)
     tipo_actividad = MultiSelectField(choices=Actividades)
     dia_atención = MultiSelectField(choices=Dias)
     hora_atención = models.CharField(max_length=15)
@@ -139,11 +173,11 @@ class DatoImpositivo(models.Model):
 
 class Cliente(models.Model):
     razon_social = models.CharField(max_length=50) # puede ser distinta de la del generador
-    domicilio_legal = models.ForeignKey('Domicilio') # (para facturacion)
-    apoderado = models.ForeignKey('Persona', related_name='apoderado')
-    contacto_comercial = models.ForeignKey('Persona', related_name='contact_comercial', null=True, blank=True)
+    domicilio_legal = models.ForeignKey('Domicilio', on_delete=models.CASCADE) # (para facturacion)
+    apoderado = models.ForeignKey('Persona', related_name='apoderado', on_delete=models.CASCADE)
+    contacto_comercial = models.ForeignKey('Persona', related_name='contact_comercial', null=True, blank=True, on_delete=models.CASCADE)
     cargo = models.CharField(max_length=50)
-    dato_impositivo = models.OneToOneField('DatoImpositivo')
+    dato_impositivo = models.OneToOneField('DatoImpositivo', on_delete=models.CASCADE)
     fecha = models.DateField(default=now) #fecha de hoy
     fecha_vinculo = models.DateField() #fecha deL Vinculo que se confeccionó el formulario
 
